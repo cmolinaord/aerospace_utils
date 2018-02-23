a601f78ed535a9a0d3c11bf345170d5237c0d28a
cmolinaord <carlosmolina.ord@gmail.com>
2018-02-23 12:59:41 +0100

    First implemented function isa_calc

--- isa_calculation.py
+++ isa_calculation.py
@@ -23,11 +23,8 @@
 import numpy as np
 from sys import argv
 
-def kelvin(T_c):
-	return T_c + 273.15
-
-def centigrade(T_k):
-	return T_k - 273.15
+##########################################
+# Objects
 
 class gas_state(object):
 	__slots__ = ["T","p","rho"]
@@ -37,6 +34,57 @@ class gas_state(object):
 		self.rho = rho
 
 ##########################################
+# Functions
+
+def kelvin(T_c):
+	return T_c + 273.15
+
+def centigrade(T_k):
+	return T_k - 273.15
+
+def isa_calc(h):
+	if h < 0:
+		print("Please give an altitude above the sea level (h>0)")
+		exit()
+
+	R = 287.0
+	g = 9.81
+
+	a = np.array([-6.5,0.0,1.0,2,8,0.0,-2.8,-2.0])
+	layer_base  = np.array([0,11000,20000,32000,47000,51000,71000])
+	layer_thick = np.diff(layer_base)
+
+	T0 = kelvin(19.0)
+	p0 = 108900.0
+
+	atm = gas_state
+	atm.T = T0
+	atm.p = p0
+
+	layer = 0
+	#h0 = layer_base[layer]
+	h1 = h
+
+	while h >= layer_base[layer]:
+		h1 = np.min([h1,layer_thick[layer]])
+		T1 = atm.T + 0.001 * a[layer] * h1
+		if a[layer] == 0:
+			p1 = atm.p * np.exp(-1 * g / R / atm.T * h1)
+		else:
+			p1 = atm.p * np.power(T1 / T0, -1000 * g / a[layer] / R)
+
+		atm.T = T1
+		atm.p = p1
+		h1 = h1 - layer_thick[layer]
+		layer += 1
+
+	# Density calculated with Gas equation
+	atm.rho = atm.p / R / atm.T
+
+	return atm, layer
+
+##########################################
+# Main
 
 if len(argv) < 2:
 	print(" ")
@@ -49,49 +97,10 @@ elif len(argv) > 2:
 	exit()
 
 h = int(argv[1])
-
-if h < 0:
-	print("Please give an altitude above the sea level (h>0)")
-	exit()
-
-print("Altitude: ", h, "m")
-
-R = 287.0
-g = 9.81
-
+atm, layer = isa_calc(h)
 name = ["Tropsosphere","Tropopause","Stratosphere","Stratosphere","Stratopause","Mesosphere","Mesosphere"]
-a = np.array([-6.5,0.0,1.0,2,8,0.0,-2.8,-2.0])
-layer_base  = np.array([0,11000,20000,32000,47000,51000,71000])
-layer_thick = np.diff(layer_base)
 
-T0 = kelvin(19.0)
-p0 = 108900.0
-
-atm = gas_state
-atm.T = T0
-atm.p = p0
-
-layer = 0
-#h0 = layer_base[layer]
-h1 = h
-
-while h >= layer_base[layer]:
-	h1 = np.min([h1,layer_thick[layer]])
-	T1 = atm.T + 0.001 * a[layer] * h1
-	if a[layer] == 0:
-		p1 = atm.p * np.exp(-1 * g / R / atm.T * h1)
-	else:
-		p1 = atm.p * np.power(T1 / T0, -1000 * g / a[layer] / R)
-
-	atm.T = T1
-	atm.p = p1
-	h1 = h1 - layer_thick[layer]
-	layer += 1
-
-# Density calculated with Gas equation
-atm.rho = atm.p / R / atm.T
-
-print("You are in the",name[layer - 1])
+print("At",h,"you are in the",name[layer - 1])
 print("  Temperature: ", np.round(atm.T,2),"K =",np.round(centigrade(atm.T),2),"degree Celsius")
 print("  Pressure: ", np.round(atm.p,2), "Pa")
 print("  Density: ", np.round(atm.rho,5), "kg/m^3")
