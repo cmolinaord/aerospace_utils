43347cda1f89337fe5b624c5af5d71a3e4124af3
cmolinaord <carlosmolina.ord@gmail.com>
2018-02-27 00:48:05 +0100

    Added class for velocity conversions

--- units.py
+++ units.py
@@ -1,5 +1,22 @@
 import const
 
+class time:
+	# Time units conversion
+	m = 60
+	h = 3600
+	day = 86400
+
+	def min2sec(t):
+		return t * 60
+
+	def hour2min(t):
+		return t * 60
+
+	def day2sec(t):
+		return t * time.day
+
+
+
 class temp:
 	# Temperature units conversion
 	def c2k(T):
@@ -9,7 +26,8 @@ class temp:
 		return T - const.T_K
 
 class dist:
-	ft = 0.3048
+	ft = 0.3048 # m
+	nmi = 1852 # Nautical mile to meters
 
 	def ft2m(d):
 		return d * dist.ft
@@ -39,3 +57,16 @@ class density:
 
 	def gm3Tokgm3(rho):
 		return 0.001 * rho
+
+class velocity:
+	def ms2kmh(vel):
+		return vel * 3.6
+
+	def kmh2ms(vel):
+		return vel / 3.6
+
+	def kn2ms(vel):
+		return vel * dist.nmi / time.h
+
+	def ms2kn(vel):
+		return vel * time.h / dist.nmi
